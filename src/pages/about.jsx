6dfe6fd3f00e80ab745578e1a3f3881bf6339679
unknown <parkerosemeyer@gmail.com>
2024-07-28 20:20:48 -0500

    Added basic link to github

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,6 +3,9 @@ const AboutPage = () => {
         <>
         <h1>About Me</h1>
         <p> About me ...</p>
+        <footer>
+            <a href='https://github.com/parros' target="_blank"><img class='aboutMeLink' src='./src/assets/images/github_logo.png' alt='Github Logo'></img></a>
+        </footer>
         </>
     )
 }
